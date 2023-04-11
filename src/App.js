c0c7c51f5200720505400e383c8ec558ee4f643a
Lee-moran <leemoran06@hotmail.com>
2023-04-11 14:29:02 +0000

    initial set up

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const url = 'https://restcountries.com/v3.1/all'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Suck It 
+      </header>
+    </div>
+  );
+}
+
+export default App;
